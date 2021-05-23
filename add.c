d87c20af5a256fbf36b39c6e27498081155cad9e
Karthik Rao <Karthikrao672@gmail.com>
2021-05-23 15:13:56 +0530

    A Simple C program

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    clrscr();
+    pritf("Enter the Numbers to add:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The Result is:%d",c);
+    getch();
+}
